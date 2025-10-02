0a265fec361e19c85b7e93c76475d328200bfaf2
Facundo-Bernard <rexlego21@gmail.com>
2025-10-02 15:43:44 +0000

    agregados links de bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
+import App from './App.jsx';
+import './index.css';
 
-import { BrowserRouter } from 'react-router-dom'
+// ----------- BOOTSTRAP IMPORTS -----------
+// CSS principal de Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// JS Bundle de Bootstrap (Popper incluido)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// Opcional: íconos de Bootstrap
+import 'bootstrap-icons/font/bootstrap-icons.css';
+// ----------------------------------------
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,4 +22,4 @@ createRoot(document.getElementById('root')).render(
       <App />
     </BrowserRouter>
   </StrictMode>
-)
+);
